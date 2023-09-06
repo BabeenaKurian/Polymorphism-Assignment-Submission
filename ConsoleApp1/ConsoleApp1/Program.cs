be3d84fc8edc9bd4679bc0ad46501c9471d43134
Babeena <babeenak020@gmail.com>
2023-09-06 18:23:58 +0530

    Polymorphism-Assignment-Submission

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable NewEmployee = new employee();
+
+            ((employee)NewEmployee).firstName = "Kitty";
+            ((employee)NewEmployee).lastName = "Katz";
+
+            NewEmployee.Quit(NewEmployee); // 
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public class employee : IQuittable // inheritance
+    {
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+       public void Quit(IQuittable Quitter) // Implementing method from interface
+        {
+            Console.WriteLine(this.firstName + " " + this.lastName + " has quit.");
+
+        }
+    }
+}
+
